fd041f6e75d35f878ca6925451feaa6c69ba4b38
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-16 11:35:39 +0530

    Created an Note model

--- server/model/Note.js
+++ server/model/Note.js
@@ -1,29 +1,35 @@
-const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
-const noteSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, 'Username is required'],
-        ref: 'User',
+const noteSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
     },
-    title: {
-        type: String,
-        required: [true, 'Password is required'],
-    },
-    text: {
-        type: String,
-        required: [true, 'Password is required'],
-    },
-    completed: {
-        type: Boolean,
-        default: false,
+    {
+        timestamps: true
     }
-}, {
-    timestamps: true,
-}
-);
+)
 
-noteSchema.plugin(AutoIncrement, { inc_field: 'ticket', id: ticketNums, start_seq: 500 });
+noteSchema.plugin(AutoIncrement, {
+    inc_field: 'ticket',
+    id: 'ticketNums',
+    start_seq: 500
+})
 
-module.exports = mongoose.model('User', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
