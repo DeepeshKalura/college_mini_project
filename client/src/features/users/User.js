63102f932ac217054937488e88aee26ce33af3e7
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-19 10:06:57 +0530

    refactor User.js

--- client/src/features/users/User.js
+++ client/src/features/users/User.js
@@ -1,23 +1,25 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
-
-import { useSelector } from "react-redux/es/hooks/useSelector";
-import { selectUserById } from "./usersApiSlice";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate } from 'react-router-dom'
+import { useGetUsersQuery } from './usersApiSlice'
+import { memo } from 'react'
 
 const User = ({ userId }) => {
-    const user = useSelector(state => selectUserById(state, userId));
+
+    const { user } = useGetUsersQuery("usersList", {
+        selectFromResult: ({ data }) => ({
+            user: data?.entities[userId]
+        }),
+    })
+
     const navigate = useNavigate()
 
     if (user) {
-        const handleEdit = () => {
-            navigate(`/dash/users/${userId}`)
-        }
+        const handleEdit = () => navigate(`/dash/users/${userId}`)
 
         const userRolesString = user.roles.toString().replaceAll(',', ', ')
 
         const cellStatus = user.active ? '' : 'table__cell--inactive'
-
 
         return (
             <tr className="table__row user">
@@ -33,7 +35,10 @@ const User = ({ userId }) => {
                 </td>
             </tr>
         )
+
     } else return null
 }
 
-export default User
+const memoizedUser = memo(User)
+
+export default memoizedUser
