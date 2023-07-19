6b23d8d5f8ee748bbe5ab5ddb7bc94fca326bd90
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-19 10:45:17 +0530

    remove express-async-handler

--- server/middleware/errorHandling.js
+++ server/middleware/errorHandling.js
@@ -1,12 +1,14 @@
-const { logEvents } = require('./logger');
+const { logEvents } = require('./logger')
 
-const errorHander = (err, req, res, next) => {
-    logEvents(`${err.name} : ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errorLog.log');
-    console.log(err.stack);
+const errorHandler = (err, req, res, next) => {
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
+    console.log(err.stack)
 
-    const status = res.statusCode ? res.statusCode : 500;
-    res.status(status);
-    res.json({ message: err.message, isError: true });
-};
+    const status = res.statusCode ? res.statusCode : 500 // server error 
 
-module.exports = { errorHander };
+    res.status(status)
+
+    res.json({ message: err.message, isError: true })
+}
+
+module.exports = errorHandler 
