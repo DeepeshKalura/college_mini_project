fccba7e9c98f80e8843cebf4e5f881f6142ba181
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-07-21 10:23:34 +0530

    Changes in link

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-8wsv.onrender.com',
+    baseUrl: 'https://technotes-api-e4gr.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
